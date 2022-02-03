e4db3bd7635eb17bb0726c30e55ac7d11974fa72
leyksnal <leyksnal@gmail.com>
2022-02-03 13:03:29 +0100

    hero header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="hero">
+        <header>
+          <div className="logo"></div>
+          <div className="nav">
+            <span>PLAY</span><span>PARENTS</span><span>EDUCATORS</span><span>WHY CODE?</span>
+          </div>
+          <div className="actionbtn">
+            <button className="join">JOIN FOR FREE</button>
+            <button className="login">LOG IN</button>
+          </div>
+        </header>
+        <div className="content-wrapper">
+          <div className="content">
+            <h2>Coding Made Easy</h2>
+            <p>The fun way to learn programming and develop problem-solving & critical thinking skills! Ages 5-18</p>
+            <button className='hero-btn'>GET STARTED FOR FREE</button>
+            <h3>At-home plans from <span className='underline'>$9 per month</span></h3>
+          </div>
+          <div className="img"></div>
+        </div>
+      </div>
+
+      <div className="info-section">
+        <div className="info-wrapper">
+          <div className="info-content">
+            <h2>The #1 Coding Platform for Kids</h2>
+          </div>
+          <div className="info-img"></div>
+        </div>
+      </div>
     </div>
   );
 }
